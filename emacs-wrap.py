135958a29097b62b0fa0c3a56659d945115c12d7
Dave Steffen <dsteffen@scitec.com>
2018-11-21 08:38:25 -0700

    Smarter emacs wrapper with emacsclient

--- emacs-wrap.py
+++ emacs-wrap.py
@@ -3,23 +3,25 @@
 import os
 import sys
 
-real_args = ['emacs']
+real_args = ['emacsclient', '-c'  ]
 for x in sys.argv[1:] :
-    if x.find(':') == -1 :
+    colon_pos = x.find(':')
+    if colon_pos == -1 :      # no colons
         if (x[-1] == '.') :
             real_args.append(x + "h")
             real_args.append(x + "cpp")
         else :
             real_args.append(x)
-    else :
+    else :                      # colon somewhere
         bits = x.split(':',1)
         line_arg = bits[1]
         filename = bits[0]
-        if line_arg[-1] == ':' :
-            line_arg = line_arg[:-1]
-        real_args.append('+' + line_arg)
+        if (len(line_arg) > 0) : # if not true, we just ignore line_arg
+            if line_arg[-1] == ':' : # strip any trailing ':'
+                line_arg = line_arg[:-1]
+            real_args.append('+' + line_arg)
         real_args.append(filename)
 
-#print real_args
+print real_args
 
-os.execvp('emacs', real_args)
+os.execvp('emacsclient', real_args)
